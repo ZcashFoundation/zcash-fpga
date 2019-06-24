7173af659b8ffd0d6658dd8da048259d494b5b7e
bsdevlin <bsdevlin@LAPTOP-AL12KDU7>
2019-06-24 23:31:09 +0800

    delete old aws files

--- aws/zcash_fpga/build/scripts/aws_build_dcp_from_cl.bat
+++
@@ -1,3 +0,0 @@
-setx VIVADO C:\Xilinx\Vivado\2018.3\bin\vivado.bat
-
-%VIVADO% -mode batch -nojournal -log vivado.log -source create_dcp_from_cl_windows.tcl -tclargs 19_04_22-123549 DEFAULT 1.4.8 0x04261818 0xF002 0x1D0F 0x1D51 0xFEDC A0 B1 C0 2 0
